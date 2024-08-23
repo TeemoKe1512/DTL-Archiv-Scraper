e7a1de152aeec4e21d9ddb587273098d758a7cdc
Timo Kemmer <timokemmer1512@gmail.com>
2024-08-23 20:39:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from modules import scrape_data, format_and_save_data
+
+
+def main():
+    base_url = 'https://www.deutsche-turnliga.de/dtl/historie/archiv/detailsm0.html?ID='
+    start_id = 2578
+    end_id = 2605
+    table_selector = 'table.Einzelnachweis.table.table-condensed'
+    date_container_selector = 'div.artikel.topbalken:has(h3.balken:has-text("Infos zum Wettkampf"))'
+
+    scrape_data(base_url, start_id, end_id, table_selector, date_container_selector)
+    format_and_save_data()
+
+if __name__ == "__main__":
+    main()
---
+++ modules/__init__.py
@@ -0,0 +1,2 @@
+from .data_scraper import scrape_data
+from .format_and_save import format_and_save_data
